42736e298095bc5a7dd8016ea93515dba1198fd7
waasnipun <waasnipun@gmail.com>
2020-06-20 00:33:49 +0530

    updated 6/20/2020

---
+++ cses/edit_distamce.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main(){
+    string a,b;
+    cin>>a>>b;
+    int n = a.size();
+    int m = b.size();
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=m;j++){
+            if(i==0){
+                dp[i][j] = j;
+                continue;
+            }
+            if(j==0){
+                dp[i][j] = i;
+                continue;
+            }
+            dp[i][j] = min(dp[i-1][j],min(dp[i-1][j-1],dp[i][j-1]))+1;
+            if(a[i-1]==b[j-1])
+                dp[i][j] = dp[i-1][j-1];
+        }
+    } 
+    cout<<dp[n][m]<<endl;
+}
